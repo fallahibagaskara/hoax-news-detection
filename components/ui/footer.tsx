0075b962f212d7cc8524d63663e6f5c4ad46f54e
Fallahi Bagaskara <fallahibagaskara@gmail.com>
2025-08-15 21:37:58 +0700

    Update Footer component to improve z-index and positioning for better layout management. This change enhances the visual hierarchy and ensures the footer remains properly positioned in relation to other elements on the page.

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <footer className="z-10 relative flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
             <p className="text-xs text-gray-500 dark:text-gray-400">© 2025 Hoax Buster. Semua hak cipta dilindungi undang-undang.</p>
             <nav className="sm:ml-auto flex gap-4 sm:gap-6">
                 <Link className="text-xs hover:underline underline-offset-4" href="/terms-of-service">
